d5ef2f18b06b8a659cfea7c535049710f868d1fe
Julian END416 <marti994@uwm.edu>
2025-11-22 11:21:48 -0600

    add Ben's bash scripts in case R code doesn't work on clusters

---
+++ bash/1_who_waves_setup.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# =====================================================
+# Script: who_waves_setup.sh
+# Purpose: Create and initialize the WAVES conda environments
+# Author: Ben Maylor (ben.maylor@ndph.ox.ac.uk)
+# Date: 23/10/2025
+# =====================================================
+
+set -e
+
+### STEPCOUNT ###
+# =====================================================
+	ENV_NAME="WHO_WAVES_stepcount"
+
+	echo "Creating conda environment: $ENV_NAME"
+	conda create -y -n $ENV_NAME python=3.9 openjdk pip
+
+	echo "Activating environment..."
+	source "$(conda info --base)/etc/profile.d/conda.sh"
+	conda activate $ENV_NAME
+
+	pip install stepcount==3.5
+	echo "Installing stepcount"
+	echo "'$ENV_NAME' setup complete!"
+# =====================================================
+
+### Actinet ###
+# =====================================================
+	ENV_NAME="WHO_WAVES_actinet"
+
+	echo "Creating conda environment: $ENV_NAME"
+	conda create -y -n $ENV_NAME python=3.9 openjdk pip
+
+	echo "Activating environment..."
+	conda activate $ENV_NAME
+
+	pip install actinet==0.4.2
+	echo "Installing actinet"
+	echo "'$ENV_NAME' setup complete!"
+# =====================================================
+
+### Accelerometer ###
+# =====================================================
+	ENV_NAME="WHO_WAVES_accelerometer"
+
+	echo "Creating conda environment: $ENV_NAME"
+	conda create -y -n $ENV_NAME python=3.9 openjdk pip
+
+	echo "Activating environment..."
+	conda activate $ENV_NAME
+
+	pip install accelerometer==7.3.0
+	echo "Installing actinet"
+	echo "'$ENV_NAME' setup complete!"
+# =====================================================
+
+echo "WAVES environments setup succesfully!"
+
+### END ###
