2a09677d122855338b09aa27fdf6a07e1fb3d3ac
Martinez <martinezj7@nih.gov>
2026-01-08 11:42:36 -0500

    wrapper_GGIR doc and include raw type

--- R/GGIR.R
+++ R/GGIR.R
@@ -1,62 +1,47 @@
-wrapper_GGIR <- function(vct_raw) {
+#' @title Run GGIR in main pipeline.
+#' @description Runs GGIR parts 1, 3 and 4 on provided raw data. Requires that
+#'  all of vct_raw are the same "raw type". If not, the function will error and
+#'  return NULL
+#' @param vct_raw Character vector of filepaths to raw data.
+#' @param vct_raw_type Character vector containing brand and data format name of
+#'  raw data.
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+wrapper_GGIR <- function(vct_raw,
+                         vct_raw_type) {
 
   if (is.null(vct_raw)) return(NULL)
 
-  # Determine average file size. If >= 1GB, its most likely field data.
-  dbl_fsize <-
-    file.size(vct_raw) |>
-    mean() / 2^30
+  all_type <- unique(vct_raw_type)
 
-  if (dbl_fsize >= 1) {
-    le_mode <-
-      1:5
-  } else {
-    le_mode <-
-      1
+  if (length(all_type) > 1) {
+
+    len_type <- length(all_type)
+    cli::cli_abort(c(
+      "More than one type of raw data is provided.",
+      "i" = "{len_type} types are data were provided.",
+      "i" = "Please provide data from one monitor brand, in one format."
+    ))
+
   }
 
-  # Determine if input is "happily read by GGIR or if user is providing a
-  # GENEActiv csv file w/ header or a no header csv.
-  # https://github.com/wadpac/GGIR/issues/518
-  I <- suppressWarnings(tryCatch(
-    GGIR::g.inspectfile(
-      datafile = vct_raw[1],
-      params_rawdata =
-        GGIR::extract_params(params2check = "rawdata")[["params_rawdata"]]
-    ),
-    error = \(e) e
-  ))
-
-  chk_adhoc_csv <-
-    I$header[1, 1] == "file does not have header" &&
-    I$sf == 0
-
-  if (class(I)[1] == "simpleError") {
-
-    chk_geneactiv_csv <-
-      I$message == "The GENEActiv csv reading functionality is deprecated in GGIR from version 2.6-4 onwards. Please, use either the GENEActiv bin files or try to read the csv files with GGIR::read.myacc.csv"
-
-    if (chk_geneactiv_csv) {
-      GGIR::GGIR(
-        mode       = le_mode,
-        datadir    = vct_raw,
-        outputdir  = "data/GGIR",
-        studyname  = "WAVES",
-        # fo         = 1,
-        # f1         = 2,
-        do.report  = c(),
-        configfile = "data/GGIR/config_WAVES_GENEActivHeaderCSV.csv"
-      )
-    } else {
-      stop(
-        "Provided csv file is not GENEActiv file or recognized by GGIR. Reach out to WAVES team.",
-        call. = FALSE
-      )
-    }
-
-  } else if (chk_adhoc_csv) {
+  if (all_type == "GENEACTIV - CSV w/ HEADER") {
     GGIR::GGIR(
-      mode       = le_mode,
+      mode       = c(1, 2, 3, 4),
+      datadir    = vct_raw,
+      outputdir  = "data/GGIR",
+      studyname  = "WAVES",
+      # fo         = 1,
+      # f1         = 2,
+      do.report  = c(),
+      configfile = "data/GGIR/config_WAVES_GENEActivHeaderCSV.csv"
+    )
+  } else if (all_type == "GENEACTIV - CSV w/o HEADER") {
+    GGIR::GGIR(
+      mode       = c(1, 2, 3, 4),
       datadir    = vct_raw,
       outputdir  = "data/GGIR",
       studyname  = "WAVES",
@@ -67,7 +52,7 @@ wrapper_GGIR <- function(vct_raw) {
     )
   } else {
     GGIR::GGIR(
-      mode       = le_mode,
+      mode       = c(1, 2, 3, 4),
       datadir    = vct_raw,
       outputdir  = "data/GGIR",
       studyname  = "WAVES",
