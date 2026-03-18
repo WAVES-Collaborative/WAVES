ab5c69249dba2725b5bcb22c5deb443e6e4b418b
Martinez <martinezj7@nih.gov>
2026-03-18 10:09:41 -0400

    hotfix/refactor - trost2017 - extract.features
    - adapt code from actimetric that returns NA for LAG1 feature during
    periods of no variability (e.g. idle sleep mode actigraph)

--- R/trost2017extended.R
+++ R/trost2017extended.R
@@ -62,29 +62,42 @@ shannon.entropy <- function(p) {
     p[p > 0] / sum(p)
   -sum(log2(p.norm) * p.norm)
 }
-extract.features <- function(Axis, wind, SampFreq) {
-  z <- wind  #window size
-  x <- 1
-  l <- floor(length(Axis)/z)
+extract.features <- function(Axis, window_size, sf) {
+  to <- window_size
+  from <- 1
+  l <- floor(length(Axis) / to)
   k <- matrix(0, nrow = l, ncol = 16)
 
   for (i in 1:l) {
-    A <- Axis[x:z]
+    A <- Axis[from:to]
     N <- length(A)
-    I <- abs(fft(A - mean(A)) / sqrt(N))^2
+    I <- abs(
+      fft(A - mean(A)) / sqrt(N)
+    )^2
     P <- (4 / N) * I
     f <- (0:(N / 2)) / N
     c <- cbind(f, P[1:(N / 2 + 1)])
-    c2 <- c[1:(wind / 10), ]
-    c3 <- c[(wind / 3 + 1):(wind / 2 + 1),]
+    c2 <- c[1:(window_size / 10), ]
+    c3 <- c[(window_size / 3 + 1):(window_size / 2 + 1), ]
+    domfreq <- as.numeric(c2[which.max(c2[, 2]), 1])
     k1 <- max(c2[, 2])
     k2 <- max(c3[, 2])
-    k3<- SampFreq * (domfreq = as.numeric(c2[which.max(c2[, 2]), 1]))
-    k4<- SampFreq * (domfreq = as.numeric(c3[which.max(c3[, 2]), 1]))
+    k3 <- sf * as.numeric(c2[which.max(c2[, 2]), 1])
+    k4 <- sf * as.numeric(c3[which.max(c3[, 2]), 1])
     k5 <- mean(A, trim = .05, na.rm = TRUE)
     k6 <- sd(A, na.rm = TRUE)
     k7 <- sum((A * A), na.rm = TRUE)
-    k8 <- cor(Axis[x:(z - 1)], Axis[(x + 1):z], use = "pairwise.complete.obs")
+    # Code for k8 adapted from actimetric package.  https://github.com/PhysicalActivityOpenTools/actimetric/blob/4b806607d54d49d8cded01563b7d61e55d24aca8/R/featuresTrost2017.R#L46
+    # this avoids warning when no variability (e.g., idle sleep mode in actigraph)
+    if (k6 != 0) {
+      k8 <- cor(
+        Axis[from:(to - 1)],
+        Axis[(from + 1):to],
+        use = "pairwise.complete.obs"
+      )
+    } else {
+      k8 <- NA
+    }
     k9 <- median(A, na.rm = TRUE)
     k10 <- mad(abs(A), na.rm = TRUE)
     k11 <- as.vector(quantile(A, c(.10), na.rm = TRUE))
@@ -92,11 +105,11 @@ extract.features <- function(Axis, wind, SampFreq) {
     k13 <- as.vector(quantile(A, c(.75), na.rm = TRUE))
     k14 <- as.vector(quantile(A, c(.90), na.rm = TRUE))
     k15 <- shannon.entropy(P)
-    k16 <- sum(c[(wind/3+1):(wind/2+1),2])
+    k16 <- sum(c[(window_size/3+1):(window_size/2+1),2])
 
     k[i,] <- c(k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15,k16)
-    x <- x + wind
-    z <- z + wind
+    from <- from + window_size
+    to <- to + window_size
   }
   return(k)
 }
