ab2b33bd41d4b536227f23cf26c051ae608f2dba
Julian END416 <marti994@uwm.edu>
2026-06-01 17:32:40 -0500

    hotfix: apply_oak.pre - strip_all_ext()

--- R/apply_methods_raw.R
+++ R/apply_methods_raw.R
@@ -749,7 +749,7 @@ apply_oak.pre <- function(fpa_read,
     basename(fpa_read)
   fnm_sans_ext <-
     basename(fpa_read) |>
-    tools::file_path_sans_ext()
+    strip_all_ext()
 
   # Check if file was already created from a previous run of the pipeline.
   fpa_write <- file.path(
